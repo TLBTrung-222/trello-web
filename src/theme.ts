c4b19f9ad592a3aa388d88a977e73d50086a3636
TrungTLB <baotrung2852003@gmail.com>
2024-08-03 23:32:37 +0700

    add custom property to theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,30 @@
 import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
+// Define the 'trello' property in the type 'CssVarsThemeOptions'
+declare module '@mui/material/styles' {
+    interface CssVarsThemeOptions {
+        trello?: {
+            appBarHeight?: string
+            boardBarHeight?: string
+        }
+    }
+    // allow configuration using `createTheme`
+    interface Theme {
+        trello: {
+            appBarHeight: string
+            boardBarHeight: string
+        }
+    }
+}
+
 // A custom theme for this app
 const theme = extendTheme({
+    // define custom property for our app
+    trello: {
+        appBarHeight: '48px',
+        boardBarHeight: '58px'
+    },
     colorSchemes: {
         light: {
             palette: {
